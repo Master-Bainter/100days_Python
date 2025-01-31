a0543b470101899bd6729325b403056def2a7124
Shawn Bainter <bainter85@gmail.com>
2025-01-30 21:35:29 -0600

    final code

--- Band_Name_Generator.py
+++ Band_Name_Generator.py
@@ -1,5 +1,6 @@
 # Code below this line
 
+print("Hey, there how's it going? Today we're going to name your band!")
 city = input("What is the name of the city you grew up in? ")
 pet = input("What is the name of your pet? ")
 
