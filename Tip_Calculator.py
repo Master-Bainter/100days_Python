5c977612fb8462c8d0fa4e0936bf93a18bc55cc0
Shawn Bainter <bainter85@gmail.com>
2025-01-30 22:00:39 -0600

    initial code for this project

---
+++ Tip_Calculator.py
@@ -0,0 +1,20 @@
+#code below this line
+
+bill = float(input("How much was the bill? "))
+tip = float(input("How much do you want to tip? 10, 12, or 15 percent?"))
+num_people = int(input("How many people will be splitting the bill? "))
+
+if tip == 10:
+    tip = .1
+elif tip == 12:
+    tip = .12
+elif tip == 15:
+    tip = .15
+else:
+    print("Sorry that isn't a selection")
+    exit()
+
+total = (bill + bill * tip) / num_people
+total = round(total,2)
+print(f"Each person should pay: ${total}")
+#print("$" + str(bill))
