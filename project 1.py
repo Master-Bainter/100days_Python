abb01084827db2804b3122621c0b8353bf5274cb
Shawn Bainter <bainter85@gmail.com>
2025-01-30 21:28:32 -0600

    changed code and added new py file

--- project 1.py
+++ project 1.py
@@ -1,2 +1,13 @@
 # Code below this line
-print("Hello " + input("What is your Name? ") + "!")
+
+glass1 = "milk"
+glass2 = "juice"
+empty_glass = ""
+
+print(glass1 + " " + glass2)
+
+empty_glass = glass1
+glass1 = glass2
+glass2 = empty_glass
+
+print(glass1 + " " + glass2)
