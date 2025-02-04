ab758ee849ff0375fced2575984cf940958f596e
Shawn Bainter <bainter85@gmail.com>
2025-02-04 14:23:43 -0600

    updated

--- project 1.py
+++ project 1.py
@@ -1,13 +1,3 @@
 # Code below this line
 
-glass1 = "milk"
-glass2 = "juice"
-empty_glass = ""
-
-print(glass1 + " " + glass2)
-
-empty_glass = glass1
-glass1 = glass2
-glass2 = empty_glass
-
-print(glass1 + " " + glass2)
+print("Number of letters in your name: " + len(int(input("Enter your name"))))
