8015249d4b033d9345f58bc0cbc625445d86c10d
Shawn Bainter <Bainter85@gmail.com>
2025-02-04 19:33:17 -0600

    updated code

--- Pizza_Order.py
+++ Pizza_Order.py
@@ -9,9 +9,34 @@ cheese_cost = 3
 
 if pizza_size == "S".lower():
     pizza_cost = 15
+    if pepperoni == "Y".lower():
+        pizza_cost = pizza_cost + pep_cost
+        if ex_cheese == "Y".lower():
+            pizza_cost = pizza_cost + cheese_cost
+        else:
+            pizza_cost = pizza_cost
+    else:
+        pizza_cost = pizza_cost
 elif pizza_size == "M".lower():
     pizza_cost = 20
+    if pepperoni == "Y".lower():
+        pizza_cost = pizza_cost + pep_cost
+        if ex_cheese == "Y".lower():
+            pizza_cost = pizza_cost + cheese_cost
+        else:
+            pizza_cost = pizza_cost
+    else:
+        pizza_cost = pizza_cost
+
 else:
     pizza_cost = 25
+    if pepperoni == "Y".lower():
+        pizza_cost = pizza_cost + pep_cost
+        if ex_cheese == "Y".lower():
+            pizza_cost = pizza_cost + cheese_cost
+        else:
+            pizza_cost = pizza_cost
+    else:
+        pizza_cost = pizza_cost
 
 print(f"${pizza_cost}")
