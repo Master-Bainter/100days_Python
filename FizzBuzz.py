19e6d098d56139b4d47eba96497b83dcbe27bb88
Shawn Bainter <bainter85@gmail.com>
2025-02-07 07:03:46 -0600

    FizzBuzz code fix

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,12 @@
 
-num = 0
+
 
 for number in range(1, 101):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
         print("Fizz")
-    else:
-        print(number)
-    if number % 5 == 0:
+    elif number % 5 == 0:
         print("Buzz")
     else:
         print(number)
