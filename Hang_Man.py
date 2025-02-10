92284ec2d210b4118895c31a09dc0f92b84cb3d2
Shawn Bainter <Bainter85@gmail.com>
2025-02-10 13:17:07 -0600

    hangman step 4

--- Hang_Man.py
+++ Hang_Man.py
@@ -1,15 +1,23 @@
 import random
 
-word_list = ['boogers', 'chess', 'burger', 'headset']
+#Allows other player to choose word to use in the game
+#Need to be carefull because word is visible to player, need to somehow mask the word
+#word = input("Choose a word for the player to guess: ")
+
+#Comment out the two lines below this and create your own random list. Need to update "word" with "word_list"
+#throughout the code
+word_list = ['boogers', 'chess', 'burger', 'headset', 'cheesecake', 'kickstart', 'desktop', 'mechanical']
 word_choice = random.choice(word_list)
+
 word_len = len(word_choice)
 placeholder = ""
 lives = 6
 game_over = False
 correct_letters = []
+wrong_letters = []
 
 #hangman stages of the game: stages is in reverse, meaning index 6 is the first display to see this
-#run a print(stages[6])
+#run a print(stages[6]).
 stages = [r'''
   +---+
   |   |
@@ -75,6 +83,7 @@ print(placeholder)
 while not game_over:
     guess = input("guess a letter: ").lower()
     display = ""
+    wrong = ""
 
     for letter in word_choice:
         if letter == guess:
@@ -86,6 +95,14 @@ while not game_over:
             display += "_"
     print(display)
 
+    if guess not in word_choice:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You Lose")
+
     if "_" not in display:
         game_over = True
         print("You Win")
+
+    print(stages[lives])
