c0c282d2c889628e81d0f79147d20abbdbd90310
Shawn Bainter <Bainter85@gmail.com>
2025-02-10 12:39:28 -0600

    hangman step 4

--- Hang_Man.py
+++ Hang_Man.py
@@ -4,19 +4,88 @@ word_list = ['boogers', 'chess', 'burger', 'headset']
 word_choice = random.choice(word_list)
 word_len = len(word_choice)
 placeholder = ""
-display = ""
-num = 0
+lives = 6
+game_over = False
+correct_letters = []
 
-print(word_choice)
-guess = input("guess a letter: ").lower()
+#hangman stages of the game: stages is in reverse, meaning index 6 is the first display to see this
+#run a print(stages[6])
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+  O   |
+ /    |
+      |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=======
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=======
+''']
 
 for position in range(word_len):
     placeholder += "_"
 print(placeholder)
 
-for letter in word_choice:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+while not game_over:
+    guess = input("guess a letter: ").lower()
+    display = ""
+
+    for letter in word_choice:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You Win")
