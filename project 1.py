fe7a684f0ecf6e631ec9a9752c4925299f6ac447
Shawn Bainter <Bainter85@gmail.com>
2025-02-10 19:52:34 -0600

    new lession

--- project 1.py
+++ project 1.py
@@ -1,7 +1,14 @@
 # Code below this line
-import Max_Function
 
+# def life_in_weeks(int):
+#     x = 52 * 90 - (52 * int)
+#     print(f"You have {x} weeks left!")
+#
+#
+# life_in_weeks(int(input("How old are you? ")))
 
-print(Max_Function)
+def greet_with_name(fname, location):
+    print(f"Hi my name is {fname} and I am from {location}")
 
+greet_with_name("Harris", "Brooklyn")
 
